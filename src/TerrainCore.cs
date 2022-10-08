e176338c6d2e07441bb954e25b6ea3f759b6ef1d
Drawtec <darkxell133@gmail.com>
2022-10-08 22:21:18 +0200

    Update TerrainCore.cs

--- src/TerrainCore.cs
+++ src/TerrainCore.cs
@@ -72,12 +72,28 @@ namespace Terrain
 			var color = Settings.TerrainColor.Value.ToRgba();
 			for (int i = 0; i < _numRows; i++)
 			{
-				for (int j = 0; j < _numCols; j += 2)
-				{
-					var b = terrainBytes[dataIndex + (j >> 1)];
-					_bitmap[k++] = (b >> 4) > 0 ? color : 0;
-					_bitmap[k++] = (b & 0xf) > 0 ? color : 0;
-				}
+                for (int j = 0; j < _numCols; j += 2)
+                {
+                    var b = LayerMelee[dataIndex + (j >> 1)];
+                    _bitmap[k++] = (b >> 4);
+                    if (_bitmap[k] < 0 && _bitmap[k] > 2)
+                    {
+                        _bitmap[k] = 0;
+                    }
+                    else
+                    {
+                        _bitmap[k] = color;
+                    }
+                    _bitmap[k++] = (b & 0xf);
+                    if (_bitmap[k] > 0)
+                    {
+                        _bitmap[k] = color;
+                    }
+                    else 
+                    { 
+                        _bitmap[k] = 0; 
+                    }
+                }
 				dataIndex += terrain.BytesPerRow;
 			}
 
